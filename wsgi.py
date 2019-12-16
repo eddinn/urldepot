05f5318348013a0b1c44550f9a00b0e39d8d82f4
Edvin Dunaway <git@eddinn.net>
2019-12-16 18:42:10 +0000

    linting wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -5,5 +5,7 @@ import logging
 
 
 logging.basicConfig(stream=sys.stderr)
-sys.path.insert(0,"/var/www/html/urldepot")
+sys.path.insert(0, "/var/www/html/urldepot")
+
+
 from urldepot import app as application
